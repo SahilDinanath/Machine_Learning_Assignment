ef4317cad5de9a14a3785e25a82be96fef29c98d
Sahil Dinanath <sahildinanath@gmail.com>
2024-05-10 16:25:06 +0200

    updated neural network structure and added gpu support to the training nn.py file

--- testing/nn.py
+++ testing/nn.py
@@ -5,6 +5,12 @@ from sklearn.metrics import accuracy_score
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+
+## trains with NVIDIA GPU if availible
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print("running on ", device)
+
 
 #####################################################################
 # splitting data step
@@ -35,44 +41,46 @@ scaler = StandardScaler()
 data = scaler.fit_transform(data)
 
 # split
-train_data = torch.tensor(data, dtype=torch.float32)
-train_labels = torch.tensor(labels, dtype=torch.long)
+train_data = torch.tensor(data, dtype=torch.float32).to(device)
+train_labels = torch.tensor(labels, dtype=torch.long).to(device)
+
+train_dataset = TensorDataset(train_data,train_labels)
 
 # architecture
 class NeuralNet(nn.Module):
     def __init__(self, input_dim, num_classes):
         super(NeuralNet, self).__init__()
         self.fc1 = nn.Linear(input_dim, 512)
-        self.fc2 = nn.Linear(512, 256)
-        self.fc3 = nn.Linear(256, 128)
-        self.fc4 = nn.Linear(128, 64)
-        self.fc5 = nn.Linear(64, num_classes)
+        self.fc2 = nn.Linear(512, 128)
+        self.fc3 = nn.Linear(128, 64)
+        self.fc4 = nn.Linear(64, num_classes)
 
     def forward(self, x):
         x = torch.relu(self.fc1(x))
         x = torch.relu(self.fc2(x))
         x = torch.relu(self.fc3(x))
-        x = torch.relu(self.fc4(x))
-        x = self.fc5(x)
+        x = self.fc4(x)
         return x
 
 # initialise nn model
-model = NeuralNet(input_dim=train_data.shape[1], num_classes=len(np.unique(labels)))
-print(len(np.unique(labels)))
+model = NeuralNet(input_dim=train_data.shape[1], num_classes=len(np.unique(labels))).to(device)
 # loss function and optimiser
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters())
 
 num_epochs = 200
-batch_size = 32
-for epoch in range(num_epochs):
-    for i in range(0, len(train_data), batch_size):
-        inputs = train_data[i:i+batch_size]
-        targets = train_labels[i:i+batch_size]
+batch_size = 64
 
+
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+
+for epoch in range(num_epochs):
+    model.train()
+    for batch_data, batch_labels in train_loader:
         optimizer.zero_grad()
-        outputs = model(inputs)
-        loss = criterion(outputs, targets)
+        outputs = model(batch_data)
+        loss = criterion(outputs, batch_labels)
         loss.backward()
         optimizer.step()
 
