fe6d53d469175be004ac62eba7dce070b4bd2279
Darshan Singh <88923579+GyuoMi@users.noreply.github.com>
2024-05-26 10:41:02 +0200

    Add files via upload

--- testing/classifyall.py
+++ testing/classifyall.py
@@ -1,67 +1,75 @@
+#  Import any Python standard libraries you wish   #
+# - I.e. libraries that do not require pip install with fresh
+#   install of Python #
+import os
+##################################
+# ALLOWED NON-STANDARD LIBRARIES #
+##################################
+# Un-comment out the ones you use
 import numpy as np
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import accuracy_score
+import pandas as pd
+import sklearn
+# import tensorflow as tf
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-# Load the test data
-# for now just train data
-#change to testdata.txt later
-test_data = np.genfromtxt("testdata.txt", delimiter=",")
+# import matplotlib
+# import seaborn as sns
+##################################
 
-scaler = StandardScaler()
-test_data = scaler.fit_transform(test_data)
-test_data = torch.tensor(test_data, dtype=torch.float32)
+def preprocessing(test_data):
+    os.system(f"python3 preprocessor.py {test_data}")
 
-
- # # architecture
-class NeuralNet(nn.Module):    
-    def __init__(self, input_dim, num_classes):
-        super(NeuralNet, self).__init__()
-        self.fc1 = nn.Linear(input_dim, 1024)
-        self.fc2 = nn.Linear(1024, 512)
-        self.fc3 = nn.Linear(512, 256)
-        self.fc4 = nn.Linear(256, num_classes)
-        self.dropout = nn.Dropout(0.3)
-        self.batch_norm1 = nn.BatchNorm1d(1024)
-        self.batch_norm2 = nn.BatchNorm1d(512)
-        self.batch_norm3 = nn.BatchNorm1d(256)
-
-    def forward(self, x):
-        x = self.dropout(F.relu(self.batch_norm1(self.fc1(x))))
-        x = self.dropout(F.relu(self.batch_norm2(self.fc2(x))))
-        x = self.dropout(F.relu(self.batch_norm3(self.fc3(x))))
-        x = self.fc4(x)
-        return x
+def main():
+    # TODO
+    # pass
+    preprocessing('testdata.txt')
+    X_test = np.array(np.genfromtxt("preprocessed_testdata.txt", delimiter=",")) 
 
 
 
+    class NeuralNet(nn.Module):
+        def __init__(self, input_dim, hidden_units, num_classes):
+            super(NeuralNet, self).__init__()
+            self.fc1 = nn.Linear(input_dim, hidden_units)
+            self.fc2 = nn.Linear(hidden_units, hidden_units)
+            self.fc3 = nn.Linear(hidden_units, hidden_units)
+            self.fc4 = nn.Linear(hidden_units, num_classes)
+            self.dropout = nn.Dropout(0.3)
+            self.batch_norm1 = nn.BatchNorm1d(hidden_units)
+            self.batch_norm2 = nn.BatchNorm1d(hidden_units)
+            self.batch_norm3 = nn.BatchNorm1d(hidden_units)
 
-# Get the input dimensions from the test data
-input_dim = test_data.shape[1]
-
-# Initialize the neural network model
-model = NeuralNet(input_dim=input_dim, num_classes=21)  
-
-# https://pytorch.org/tutorials/beginner/saving_loading_models.html
-model.load_state_dict(torch.load("pretrained_model.pth", map_location=torch.device('cpu')))
-
-# Make predictions on the test data
-with torch.no_grad():
-    outputs = model(test_data)
-    _, predicted_labels = torch.max(outputs, 1)
-
-# Save predicted labels to output file
-np.savetxt("predlabels.txt", predicted_labels.numpy(), delimiter='\n',fmt="%d")
+        def forward(self, x):
+            x = self.dropout(F.relu(self.batch_norm1(self.fc1(x))))
+            x = self.dropout(F.relu(self.batch_norm2(self.fc2(x))))
+            x = self.dropout(F.relu(self.batch_norm3(self.fc3(x))))
+            x = self.fc4(x)
+            return x
 
 
+    models = {}
+    for category in range(4):
+        model_data = torch.load(f"best_model_cat_{category}.pth")
+        model = NeuralNet(model_data["input_dim"], model_data['hidden_units'], model_data['class_dim'])
+        model.load_state_dict(model_data['model_state_dict'])
+        model.eval()
+        models[category] = model
 
-###remove later 
-true_labels = np.genfromtxt("testlabels.txt", delimiter="\n")
-predicted_labels = np.genfromtxt("predlabels.txt", delimiter="\n")
+    predictions = []
 
-print(
-    "Model accuracy: {:.2f}%".format(
-        accuracy_score(true_labels, predicted_labels) * 100
-    )
-)
+    for row in X_test:
+        category = int(row[-1])
+        data_point = torch.from_numpy(row[:-1].astype(np.float32)).unsqueeze(0)  # Add batch dimension
+
+        model = models[category]
+
+        with torch.no_grad():
+            output = model(data_point)
+            _, predicted = torch.max(output, 1)
+            predictions.append(predicted.item())
+
+    np.savetxt("predlabels.txt", predictions, fmt='%d')
+
+if __name__ == "__main__":
+    main()
--- testing/preprocessor.py
+++ testing/preprocessor.py
@@ -1,39 +1,44 @@
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
+import sys
 
-data = pd.read_csv('traindata.txt', header=None)
-rotations = data.iloc[:, -1]
-data = data.iloc[:, :-1]
+def preprocess(filename):
+    data = pd.read_csv(f'{filename}', header=None)
+    rotations = data.iloc[:, -1]
+    data = data.iloc[:, :-1]
 
-# add 256 to all negatives
-# negative_columns = data.columns[(data < 0).any()]
-# data[negative_columns] = data[negative_columns] 
-# remove negative columns
-data = data.loc[:, (data >= 0).all()]
-# remove columns greater then 255
-#data = data.loc[:, (data <=255).all()]
-# scale between 0 - 255
-scaler = MinMaxScaler(feature_range=(0, 255))
+    # add 256 to all negatives
+    # negative_columns = data.columns[(data < 0).any()]
+    # data[negative_columns] = data[negative_columns] 
+    # remove negative columns
+    data = data.loc[:, (data >= 0).all()]
+    # remove columns greater then 255
+    #data = data.loc[:, (data <=255).all()]
+    # scale between 0 - 255
+    scaler = MinMaxScaler(feature_range=(0, 255))
 
-columns_to_scale = data.columns[(data.max() > 255)]
-data[columns_to_scale] = scaler.fit_transform(data[columns_to_scale])
+    columns_to_scale = data.columns[(data.max() > 255)]
+    data[columns_to_scale] = scaler.fit_transform(data[columns_to_scale])
 
-# columns_with_255 = data.columns[data.isin([255]).any()]
-# data = data[columns_with_255]
-#
+    # columns_with_255 = data.columns[data.isin([255]).any()]
+    # data = data[columns_with_255]
+    #
 
-# log transformation either use this or the scaler (can use both, need to test more to find a proper answer)
-#high_value_columns = data.columns[(data.max() > 255)]
-#data[high_value_columns] = data[high_value_columns].apply(lambda x: np.log1p(x))
+    # log transformation either use this or the scaler (can use both, need to test more to find a proper answer)
+    #high_value_columns = data.columns[(data.max() > 255)]
+    #data[high_value_columns] = data[high_value_columns].apply(lambda x: np.log1p(x))
 
-# this is dogshit
-# scaler = StandardScaler()
-# data = pd.DataFrame(scaler.fit_transform(data), columns=data.columns)
+    # this is dogshit
+    # scaler = StandardScaler()
+    # data = pd.DataFrame(scaler.fit_transform(data), columns=data.columns)
 
-# create a new dataframe since otherwise we get fragmentation due to calling a dataframe a lot
-rotations = pd.DataFrame(rotations.values, columns=['rotations'])
-data = pd.concat([data, rotations], axis=1)
-data.to_csv('preprocessed_traindata.txt', index=False, header=False)
+    # create a new dataframe since otherwise we get fragmentation due to calling a dataframe a lot
+    rotations = pd.DataFrame(rotations.values, columns=['rotations'])
+    data = pd.concat([data, rotations], axis=1)
+    data.to_csv(f'preprocessed_{filename}', index=False, header=False)
 
 
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    preprocess(filename)
